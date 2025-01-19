d1a681f74a34b52877ba6e43444460e0c48916a2
amar-malakar <amarmalakarinfo@gmail.com>
2025-01-19 11:09:51 +0530

    customize the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button>CLICK ME!</Button>
-      <Button>CLICK ME!</Button>
-      <Calendar />
+    <div className="">
+      <div className="flex gap-4">
+        <Button>Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="destructive">Destructive</Button>
+        <Button variant="ghost">Ghost</Button>
+        <Button variant="muted">Muted</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="teritary">Teritary</Button>
+        <Button disabled>Teritary</Button>
+      </div>
+      <Input />
       <p className="text-red-500 font-semibold">Amar</p>
     </div>
   );
